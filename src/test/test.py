3e719a008a0ec6ff15c054f5bfaf5a373753c893
coderRaj07 <12131501.edu@gmail.com>
2025-05-04 12:34:21 +0530

    feat: added test folders

---
+++ src/test/test.py
@@ -0,0 +1,8 @@
+print("Code is working")
+
+# python3 -m src.test.extra_column_csv_generated_data
+# python3 -m src.test.generate_csv_data
+# python3 -m src.test.generate_customer_table_data
+# python3 -m src.test.generate_datewise_sales_data
+# python3 -m src.test.less_column_csv_generated_data
+# python3 -m src.test.less_column_csv_generated_data
