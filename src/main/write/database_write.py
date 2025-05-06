8a45d5a561392a83bbec454297299edec5d3225a
coderRaj07 <12131501.edu@gmail.com>
2025-05-06 18:22:03 +0530

    feat: comments removed

--- src/main/write/database_write.py
+++ src/main/write/database_write.py
@@ -4,21 +4,7 @@ from src.main.utility.logging_config import *
 from src.main.utility.pg_sql_session import get_pgsql_connection
 
 class DatabaseWriter:
-    # def __init__(self,url,properties):
-    #     self.url = url
-    #     self.properties = properties
-
-    # def write_dataframe(self,df,table_name):
-    #     try:
-    #         print("inside write_dataframe")
-    #         df.write.jdbc(url=self.url,
-    #                       table=table_name,
-    #                       mode="append",
-    #                       properties=self.properties)
-    #         logger.info(f"Data successfully written into {table_name} table ")
-    #     except Exception as e:
-    #         return {f"Message: Error occured {e}"}
-
+    
     def write_file_info_to_pgsql(self, file_info):
         """
         Write file metadata to PostgreSQL using psycopg2. 
